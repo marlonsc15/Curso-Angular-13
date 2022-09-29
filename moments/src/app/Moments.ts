64d62d40e89bb3374f993bd70336d3a4141c0f1d
marlonsc15 <marlonsantoscorreia@gmail.com>
2022-09-29 16:33:48 -0300

    pagina home , footer e header

---
+++ moments/src/app/Moments.ts
@@ -0,0 +1,8 @@
+export interface Moment {
+    id?: number,
+    title: string,
+    description: string,
+    image: string,
+    created_at?: string,
+    update_at?: string,
+}
--- moments/src/app/app-routing.module.ts
+++ moments/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
+import { NewMomentsComponent } from './components/pages/new-moments/new-moments.component';
+import { AboutComponent } from './components/pages/about/about.component';
+import { HomeComponent } from './components/pages/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', 
+    component: HomeComponent,  
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+  {
+    path: 'moments/new', 
+    component: NewMomentsComponent,  
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
