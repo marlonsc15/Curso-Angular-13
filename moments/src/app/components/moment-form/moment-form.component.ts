46b8517ea2f3acf936657605a1ced7eb9f95c69d
marlonsc15 <marlonsantoscorreia@gmail.com>
2022-09-29 18:20:18 -0300

    finalização da confirmação de momento

--- moments/src/app/components/moment-form/moment-form.component.ts
+++ moments/src/app/components/moment-form/moment-form.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-moment-form',
@@ -6,11 +7,34 @@ import { Component, Input, OnInit } from '@angular/core';
   styleUrls: ['./moment-form.component.css']
 })
 export class MomentFormComponent implements OnInit {
-  @Input() btnText!: string
+  @Input() btnText!: string;
+
+  momentForm! : FormGroup
 
   constructor() { }
 
   ngOnInit(): void {
+    this.momentForm = new FormGroup({
+      id: new FormControl(''),
+      title: new FormControl('', [Validators.required]),
+      description: new FormControl('', [Validators.required]),
+    })
+  }
+
+  get title() {
+    return this.momentForm.get('title')!;
+  }
+  get description() {
+    return this.momentForm.get('description')!;
+  }
+
+  submit() {
+
+    if (this.momentForm.invalid) {
+      return;
+    }
+    console.log('enviou formulario');
+    
   }
 
 }
